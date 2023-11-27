4a37f12dceaa163e6a17f3fb07eb5aaa8ed848f0
Paras Sawale <147709064+parassawale@users.noreply.github.com>
2023-11-27 22:58:56 +0530

    Create                 Static-member-function.cpp

---
+++ Static-member-function.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+// #is preprocessor and standard input output. //It provides the declaration for (input/output
+
+stream).
+
+class My class
+
+// Class name
+
+{
+
+Public:
+
+// Modifier which access the level of a class member.
+
+Static void static function ()
+
+// Declaration of name of the static member function.
+
+{
+
+Std:: cout <<"this is a static member function"<<std::endl;
+
+and // Printing the statement }
+
+int main()
+
+{
+
+My class:: static function ();
+
+//Name of class,then function name is used
+
+returnO;
+
+// Terminates successfully
